bfcf638abc58429f9616366dd60a93cdfac0b4e6
Shoham Liebermann <shoham@Shohams-MacBook-Pro.local>
2026-02-04 16:27:53 +0200

    Add manual service worker for Vercel PWA

--- src/components/RegisterSW.tsx
+++ src/components/RegisterSW.tsx
@@ -5,7 +5,6 @@ import { useEffect } from "react";
 export default function RegisterSW() {
   useEffect(() => {
     if (!("serviceWorker" in navigator)) return;
-
     navigator.serviceWorker.register("/sw.js", { scope: "/" }).catch(() => {});
   }, []);
 
